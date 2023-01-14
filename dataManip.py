bf2c9f352fec4037804348fb8cb980fe4c5d17b5
chrisowski1 <35178770+chrisowski1@users.noreply.github.com>
2023-01-14 11:16:12 -0500

    add data manipulation and plotting

---
+++ dataManip.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame({'Red Meat': ['100 pounds', '89 pounds', '75 pounds'],
+                   'Grains': ['2 pounds', '167 pounds', '98 pounds'],
+                   'Dairy': ['200 pounds', '42 pounds', '135 pounds'],
+                   'Cellphone': ['55 hours', '42 hours', '75 hours'],
+                   'TV': ['43 hours', '75 hours', '88 hours'],
+                   'Computer': ['130 hours', '88 hours', '102 hours'],
+                   })
+
+def convert_to_kg(weight):
+    return round(float(weight.split()[0]) * 0.453592, 2)
+
+df["Red Meat"] = df["Red Meat"].apply(convert_to_kg)
+df["Grains"] = df["Grains"].apply(convert_to_kg)
+df["Dairy"] = df["Dairy"].apply(convert_to_kg)
+
+df.rename(columns=lambda x: x.replace("pounds","kg"), inplace=True)
+
+df = df.replace(to_replace = " hours", value = "", regex = True)
+
+df[['Red Meat','Grains','Dairy','Cellphone','TV','Computer']] = df[['Red Meat','Grains','Dairy','Cellphone','TV','Computer']].astype(float)
+
+print(df)
+
+
+food_df = df[['Red Meat','Grains','Dairy']]
+electronics_df = df[['Cellphone','TV','Computer']]
+
+# Create two subplots
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+# Plot the food data
+food_df.plot(kind='bar',ax=axs[0])
+axs[0].set_xlabel('Index')
+axs[0].set_ylabel('Weight')
+axs[0].set_title('Weight of food items')
+
+# Plot the electronics data
+electronics_df.plot(kind='bar',ax=axs[1])
+axs[1].set_xlabel('Index')
+axs[1].set_ylabel('Usage')
+axs[1].set_title('Usage of electronic devices')
+
+plt.show()
+
+
+
