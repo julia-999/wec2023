1d88f03e0ed33cd336d60f8124292e272a352551
Julia Anantchenko <janantch@uwo.ca>
2023-01-14 11:28:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,33 +13,43 @@ with open('file1.txt') as file:
     for i in range(0, len(lines)):
         # print(line)
         line = lines[i]
-        print(line)
+        # print(line)
         if line.startswith('='):
             if str(line).strip('= ').startswith('Request'):
-                print("request")
+                # print("request")
                 arr = '{"Type": "Request"'
-                while lines[i+1] != '\n':
-                    i = i+1
-                    line = lines[i].strip('\n')
-                    split = str(line).split(": ")
-                    # print(split)
+            if str(line).strip('= ').startswith('Response'):
+                # print("response")
+                arr = '{"Type": "Response"'
+
+            while lines[i+1] != '\n':
+                i = i+1
+                line = lines[i].strip('\n')
+                split = str(line).split(": ")
+                # print(split)
+                try:
                     arr += ', "' + split[0] + '":"' + split[1] + '"'
-                arr += '}'
-                # print(arr)
-                arr = json.loads(arr)
-                dfnew = pd.DataFrame.from_dict(arr, orient="index")
-                # dfnew = pd.pivot_table(dfnew)
-                dfnew = dfnew.T
-                print("here")
-                print(dfnew)
-                df = pd.concat([df, dfnew])
-                print("here2")
-                print(df)
+                except:
+                    print("ERROR" + str(split))
+                    split = str(line).split('\t')
+                    arr += ', "' + split[0] + '":"' + split[1] + '"'
+            arr += '}'
+            # print(arr)
+            arr = json.loads(arr)
+            dfnew = pd.DataFrame.from_dict(arr, orient="index")
+            # dfnew = pd.pivot_table(dfnew)
+            dfnew = dfnew.T
+            # print("here")
+            # print(dfnew)
+            df = pd.concat([df, dfnew])
+            # print("here2")
+            # print(df)
 
 
-            if line.strip('= ').startswith('Response'):
-                print("response")
-    # print(df)
+
+
+
+    print(df)
 
 
 # %%
